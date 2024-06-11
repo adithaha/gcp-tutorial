c6ae0fd047e02b82b7016c1f1d0c58fd0ead0822
adithaha <adit.haha@gmail.com>
2024-06-11 10:11:26 +0700

    Create text-summaryze.sh

---
+++ gen-ai/gemini/text-summaryze.sh
@@ -0,0 +1,5 @@
+curl -X POST \
+     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+     -H "Content-Type: application/json; charset=utf-8" \
+     -d @text-summarize.json \
+     "https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/gemini-1.5-pro:streamGenerateContent?alt=sse"
